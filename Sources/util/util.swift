da1cec690f4700f4511b6aaf1c8fa1ba270195d2
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2025-06-22 12:31:20 +0530

    implemented Json Serialization Deserialization

--- Sources/util/util.swift
+++ Sources/util/util.swift
@@ -1,2 +1,100 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+/**
+ * @author Yuvraj Singh
+ */
+enum JsonException: Error, LocalizedError {
+    case encodingError(Error)
+    case decodingError(Error)
+    case conversionError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .encodingError(let error):
+            return "Could not encode: \(error.localizedDescription)"
+        case .decodingError(let error):
+            return "Could not decode: \(error.localizedDescription)"
+        case .conversionError(let error):
+            return "Could not convert: \(error.localizedDescription)"
+        }
+    }
+}
+
+/**
+ * @author Yuvraj Singh
+ */
+protocol Json {
+    func encode(_ object: Any) throws -> String
+    func decode<T: Decodable>(_ json: String, as type: T.Type) throws -> T
+    func convert<T: Decodable>(_ object: Any, to type: T.Type) throws -> T
+    static func create() -> Json
+}
+
+/**
+ * @author Yuvraj Singh
+ */
+class DefaultJson: Json {
+    private let encoder = JSONEncoder()
+    private let decoder: JSONDecoder
+    
+    init() {
+        let decoder = JSONDecoder()
+        // Swift's JSONDecoder ignores unknown properties by default
+        self.decoder = decoder
+    }
+    
+    func encode(_ object: Any) throws -> String {
+        guard let encodable = object as? Encodable else {
+            throw JsonException.encodingError(NSError(domain: "Not Encodable", code: 0))
+        }
+        do {
+            let data = try encoder.encode(AnyEncodable(encodable))
+            guard let json = String(data: data, encoding: .utf8) else {
+                throw JsonException.encodingError(NSError(domain: "Encoding to String failed", code: 0))
+            }
+            return json
+        } catch {
+            throw JsonException.encodingError(error)
+        }
+    }
+    
+    func decode<T: Decodable>(_ json: String, as type: T.Type) throws -> T {
+        guard let data = json.data(using: .utf8) else {
+            throw JsonException.decodingError(NSError(domain: "Invalid UTF-8", code: 0))
+        }
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            throw JsonException.decodingError(error)
+        }
+    }
+    
+    func convert<T: Decodable>(_ object: Any, to type: T.Type) throws -> T {
+        do {
+            let json = try encode(object)
+            return try decode(json, as: type)
+        } catch {
+            throw JsonException.conversionError(error)
+        }
+    }
+    
+    static func create() -> Json {
+        return DefaultJson()
+    }
+}
+
+// Helper to allow encoding of Any Encodable
+struct AnyEncodable: Encodable {
+    private let encodable: Encodable
+    
+    init(_ encodable: Encodable) {
+        self.encodable = encodable
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        try encodable.encode(to: encoder)
+    }
+}
--- Tests/utilTests/utilTests.swift
+++ Tests/utilTests/utilTests.swift
@@ -1,6 +1,30 @@
-import Testing
-@testable import util
+import XCTest
+@testable import util  // 'util' must match your Swift module/package name exactly
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+/**
+ * @author Yuvraj Singh
+ */
+final class JsonTests: XCTestCase {
+    struct User: Codable, Equatable {
+        let name: String
+        let age: Int
+    }
+
+    func testEncodingAndDecoding() {
+        let jsonUtil = DefaultJson()
+            let user = User(name: "Yuvraj", age: 30)
+
+            do {
+                let encoded = try jsonUtil.encode(user)
+                print("Encoded JSON:", encoded)
+                XCTAssertFalse(encoded.isEmpty, "Encoded string should not be empty")
+
+                let decoded: User = try jsonUtil.decode(encoded, as: User.self)
+                print("Decoded:", decoded)
+                XCTAssertEqual(decoded, user, "Decoded user should match the original")
+            } catch {
+                XCTFail("Test failed with error: \(error)")
+            }
+    }
 }
+
